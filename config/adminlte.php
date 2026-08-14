affb2e9b3209b728b55530248607f51be38fb059
vixionrianto-ai <vixionrianto@gmail.com>
2026-08-14 07:32:41 +0700

    Add Role Management to admin sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -2,92 +2,25 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Title
-    |--------------------------------------------------------------------------
-    |
-    | Here you can change the default title of your admin panel.
-    |
-    | For detailed instructions you can look the title section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
-    |
-    */
-
     'title' => 'GNS Billing',
-
     'title_prefix' => '',
-
     'title_postfix' => ' | GNS Network',
-    /*
-    |--------------------------------------------------------------------------
-    | Favicon
-    |--------------------------------------------------------------------------
-    |
-    | Here you can activate the favicon.
-    |
-    | For detailed instructions you can look the favicon section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
-    |
-    */
 
     'use_ico_only' => false,
     'usermenu_image' => true,
     'use_full_favicon' => false,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Google Fonts
-    |--------------------------------------------------------------------------
-    |
-    | Here you can allow or not the use of external google fonts. Disabling the
-    | google fonts may be useful if your admin panel internet access is
-    | restricted somehow.
-    |
-    | For detailed instructions you can look the google fonts section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
-    |
-    */
-
     'google_fonts' => [
         'allowed' => true,
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Admin Panel Logo
-    |--------------------------------------------------------------------------
-    |
-    | Here you can change the logo of your admin panel.
-    |
-    | For detailed instructions you can look the logo section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
-    |
-    */
 
     'logo' => '<b>GNS</b> Network
     <br><small style="font-size:11px;font-weight:400;">
     Billing Management System
     </small>',
-
     'logo_img' => 'images/logo.png',
-
     'logo_img_class' => 'brand-image img-circle elevation-3',
-
     'logo_img_alt' => 'GNS Network',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Logo
-    |--------------------------------------------------------------------------
-    |
-    | Here you can setup an alternative logo to use on your login and register
-    | screens. When disabled, the admin panel logo will be used instead.
-    |
-    | For detailed instructions you can look the auth logo section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
-    |
-    */
 
     'auth_logo' => [
         'enabled' => false,
@@ -100,76 +33,23 @@ return [
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Preloader Animation
-    |--------------------------------------------------------------------------
-    |
-    | Here you can change the preloader animation configuration. Currently, two
-    | modes are supported: 'fullscreen' for a fullscreen preloader animation
-    | and 'cwrapper' to attach the preloader animation into the content-wrapper
-    | element and avoid overlapping it with the sidebars and the top navbar.
-    |
-    | For detailed instructions you can look the preloader section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
-    |
-    */
-
     'preloader' => [
-
         'enabled' => true,
-
         'mode' => 'fullscreen',
-
         'img' => [
-
             'path' => 'images/logo.png',
-
             'alt' => 'GNS Network',
-
             'effect' => 'animation__pulse',
-
             'width' => 90,
-
             'height' => 90,
-
         ],
-
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | User Menu
-    |--------------------------------------------------------------------------
-    |
-    | Here you can activate and change the user menu.
-    |
-    | For detailed instructions you can look the user menu section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
-    |
-    */
-
     'usermenu_enabled' => true,
-
     'usermenu_header' => true,
-
     'usermenu_header_class' => 'bg-primary',
-
     'usermenu_image' => true,
-
     'usermenu_desc' => 'Administrator GNS',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Layout
-    |--------------------------------------------------------------------------
-    |
-    | Here we change the layout of your admin panel.
-    |
-    | For detailed instructions you can look the layout section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
-    |
-    */
 
     'layout_topnav' => null,
     'layout_boxed' => null,
@@ -178,36 +58,12 @@ return [
     'layout_fixed_footer' => null,
     'layout_dark_mode' => false,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Views Classes
-    |--------------------------------------------------------------------------
-    |
-    | Here you can change the look and behavior of the authentication views.
-    |
-    | For detailed instructions you can look the auth classes section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
-    |
-    */
-
     'classes_auth_card' => 'card-outline card-primary',
     'classes_auth_header' => '',
     'classes_auth_body' => '',
     'classes_auth_footer' => '',
     'classes_auth_icon' => '',
     'classes_auth_btn' => 'btn-flat btn-primary',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Admin Panel Classes
-    |--------------------------------------------------------------------------
-    |
-    | Here you can change the look and behavior of the admin panel.
-    |
-    | For detailed instructions you can look the admin panel classes here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
-    |
-    */
 
     'classes_body' => '',
     'classes_brand' => '',
@@ -221,18 +77,6 @@ return [
     'classes_topnav_nav' => 'navbar-expand',
     'classes_topnav_container' => 'container',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Sidebar
-    |--------------------------------------------------------------------------
-    |
-    | Here we can modify the sidebar of the admin panel.
-    |
-    | For detailed instructions you can look the sidebar section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
-    |
-    */
-
     'sidebar_mini' => 'lg',
     'sidebar_collapse' => false,
     'sidebar_collapse_auto_size' => false,
@@ -243,18 +87,6 @@ return [
     'sidebar_nav_accordion' => true,
     'sidebar_nav_animation_speed' => 300,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Control Sidebar (Right Sidebar)
-    |--------------------------------------------------------------------------
-    |
-    | Here we can modify the right sidebar aka control sidebar of the admin panel.
-    |
-    | For detailed instructions you can look the right sidebar section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Layout-and-Styling-Configuration
-    |
-    */
-
     'right_sidebar' => false,
     'right_sidebar_icon' => 'fas fa-cogs',
     'right_sidebar_theme' => 'dark',
@@ -262,18 +94,6 @@ return [
     'right_sidebar_push' => true,
     'right_sidebar_scrollbar_theme' => 'os-theme-light',
     'right_sidebar_scrollbar_auto_hide' => 'l',
-
-    /*
-    |--------------------------------------------------------------------------
-    | URLs
-    |--------------------------------------------------------------------------
-    |
-    | Here we can modify the url settings of the admin panel.
-    |
-    | For detailed instructions you can look the urls section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Basic-Configuration
-    |
-    */
 
     'use_route_url' => true,
     'dashboard_url' => 'dashboard',
@@ -285,190 +105,138 @@ return [
     'profile_url' => false,
     'disable_darkmode_routes' => false,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel Asset Bundling
-    |--------------------------------------------------------------------------
-    |
-    | Here we can enable the Laravel Asset Bundling option for the admin panel.
-    | Currently, the next modes are supported: 'mix', 'vite' and 'vite_js_only'.
-    | When using 'vite_js_only', it's expected that your CSS is imported using
-    | JavaScript. Typically, in your application's 'resources/js/app.js' file.
-    | If you are not using any of these, leave it as 'false'.
-    |
-    | For detailed instructions you can look the asset bundling section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Other-Configuration
-    |
-    */
-
     'laravel_asset_bundling' => false,
     'laravel_css_path' => 'css/app.css',
     'laravel_js_path' => 'js/app.js',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Menu Items
-    |--------------------------------------------------------------------------
-    |
-    | Here we can modify the sidebar/top navigation of the admin panel.
-    |
-    | For detailed instructions you can look here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Menu-Configuration
-    |
-    */
+    'menu' => [
 
-    
+        ['header' => 'MASTER DATA'],
 
-    /*
-    |--------------------------------------------------------------------------
-    | TOP NAVBAR
-    |--------------------------------------------------------------------------
-    */
-'menu' => [
+        [
+            'text' => 'Router',
+            'route' => 'router.index',
+            'icon' => 'fas fa-network-wired',
+        ],
+        [
+            'text' => 'Paket Internet',
+            'route' => 'paket.index',
+            'icon' => 'fas fa-wifi',
+        ],
+        [
+            'text' => 'Pelanggan',
+            'route' => 'pelanggan.index',
+            'icon' => 'fas fa-users',
+        ],
 
+        ['header' => 'TRANSAKSI'],
 
-    ['header' => 'MASTER DATA'],
+        [
+            'text' => 'Tagihan',
+            'route' => 'tagihan.index',
+            'icon' => 'fas fa-file-invoice',
+        ],
+        [
+            'text' => 'Pembayaran',
+            'route' => 'pembayaran.index',
+            'icon' => 'fas fa-money-check-alt',
+        ],
+        [
+            'text' => 'Riwayat WhatsApp',
+            'route' => 'whatsapp.index',
+            'icon' => 'fab fa-whatsapp',
+        ],
 
-    [
-        'text'  => 'Router',
-        'route' => 'router.index',
-        'icon'  => 'fas fa-network-wired',
+        ['header' => 'LAPORAN'],
+
+        [
+            'text' => 'Dashboard Analitik',
+            'route' => 'dashboard',
+            'icon' => 'fas fa-chart-line',
+        ],
+        [
+            'text' => 'Laporan',
+            'route' => 'laporan.index',
+            'icon' => 'fas fa-chart-bar',
+        ],
+
+        ['header' => 'MIKROTIK'],
+
+        [
+            'text' => 'Monitoring MikroTik',
+            'route' => 'mikrotik.monitor',
+            'icon' => 'fas fa-network-wired',
+        ],
+
+        ['header' => 'SYSTEM'],
+
+        [
+            'text' => 'User Management',
+            'route' => 'users.index',
+            'icon' => 'fas fa-users-cog',
+            'can' => 'user.view',
+        ],
+
+        [
+            'text' => 'Role Management',
+            'route' => 'roles.index',
+            'icon' => 'fas fa-user-shield',
+            'can' => 'role.view',
+        ],
+
+        [
+            'text' => 'Audit Trail',
+            'route' => 'audit.index',
+            'icon' => 'fas fa-history',
+        ],
+        [
+            'text' => 'Pengaturan',
+            'route' => 'settings.index',
+            'icon' => 'fas fa-cogs',
+        ],
+        [
+            'text' => 'Profile',
+            'route' => 'profile.edit',
+            'icon' => 'fas fa-user',
+        ],
+
+        ['header' => 'SUPER ADMIN'],
+
+        [
+            'text' => 'Backup Database',
+            'url' => '#',
+            'icon' => 'fas fa-database',
+        ],
+        [
+            'text' => 'Restore Database',
+            'url' => '#',
+            'icon' => 'fas fa-upload',
+        ],
+        [
+            'text' => 'Reset Data',
+            'route' => 'superadmin.index',
+            'icon' => 'fas fa-trash-alt',
+        ],
+        [
+            'text' => 'Factory Reset',
+            'url' => '#',
+            'icon' => 'fas fa-radiation',
+        ],
+        [
+            'text' => 'Informasi Sistem',
+            'url' => '#',
+            'icon' => 'fas fa-info-circle',
+        ],
+
+        ['header' => 'ACCOUNT'],
+
+        [
+            'text' => 'Logout',
+            'url' => 'logout',
+            'method' => 'post',
+            'icon' => 'fas fa-sign-out-alt',
+        ],
     ],
-
-    [
-        'text' => 'Paket Internet',
-        'route' => 'paket.index',
-        'icon' => 'fas fa-wifi',
-    ],
-
-    [
-        'text' => 'Pelanggan',
-        'route' => 'pelanggan.index',
-        'icon' => 'fas fa-users',
-    ],
-
-    ['header' => 'TRANSAKSI'],
-
-    [
-        'text' => 'Tagihan',
-        'route' => 'tagihan.index',
-        'icon' => 'fas fa-file-invoice',
-    ],
-
-    [
-        'text' => 'Pembayaran',
-        'route' => 'pembayaran.index',
-        'icon' => 'fas fa-money-check-alt',
-    ],
-
-    [
-        'text'  => 'Riwayat WhatsApp',
-        'route' => 'whatsapp.index',
-        'icon'  => 'fab fa-whatsapp',
-    ],
-
-    ['header' => 'LAPORAN'],
-
-    [
-        'text' => 'Dashboard Analitik',
-        'route' => 'dashboard',
-        'icon' => 'fas fa-chart-line',
-    ],
-
-    [
-        'text' => 'Laporan',
-        'route' => 'laporan.index',
-        'icon' => 'fas fa-chart-bar',
-    ],
-
-    ['header' => 'MIKROTIK'],
-
-    [
-        'text' => 'Monitoring MikroTik',
-        'route' => 'mikrotik.monitor',
-        'icon' => 'fas fa-network-wired',
-    ],
-
-    ['header' => 'SYSTEM'],
-
-    [
-        'text' => 'User Management',
-        'route' => 'users.index',
-        'icon' => 'fas fa-users-cog',
-        'can' => 'user.view',
-    ],
-
-    [
-        'text' => 'Audit Trail',
-        'route' => 'audit.index',
-        'icon' => 'fas fa-history',
-    ],
-
-    [
-        'text' => 'Pengaturan',
-        'route' => 'settings.index',
-        'icon' => 'fas fa-cogs',
-    ],
-
-    [
-        'text' => 'Profile',
-        'route' => 'profile.edit',
-        'icon' => 'fas fa-user',
-    ],
-
-    ['header' => 'SUPER ADMIN'],
-
-    [
-        'text' => 'Backup Database',
-        'url' => '#',
-        'icon' => 'fas fa-database',
-    ],
-
-    [
-        'text' => 'Restore Database',
-        'url' => '#',
-        'icon' => 'fas fa-upload',
-    ],
-
-    [
-        'text' => 'Reset Data',
-        'route' => 'superadmin.index',
-        'icon' => 'fas fa-trash-alt',
-    ],
-
-    [
-        'text' => 'Factory Reset',
-        'url' => '#',
-        'icon' => 'fas fa-radiation',
-    ],
-
-    [
-        'text' => 'Informasi Sistem',
-        'url' => '#',
-        'icon' => 'fas fa-info-circle',
-    ],
-
-    ['header' => 'ACCOUNT'],
-
-    [
-        'text' => 'Logout',
-        'url' => 'logout',
-        'method' => 'post',
-        'icon' => 'fas fa-sign-out-alt',
-    ],
-],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Menu Filters
-    |--------------------------------------------------------------------------
-    |
-    | Here we can modify the menu filters of the admin panel.
-    |
-    | For detailed instructions you can look the menu filters section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Menu-Configuration
-    |
-    */
 
     'filters' => [
         JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
@@ -480,19 +248,6 @@ return [
         JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class,
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Plugins Initialization
-    |--------------------------------------------------------------------------
-    |
-    | Here we can modify the plugins used inside the admin panel.
-    |
-    | For detailed instructions you can look the plugins section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Plugins-Configuration
-    |
-    */
-
-    
     'plugins' => [
         'Datatables' => [
             'active' => false,
@@ -566,19 +321,6 @@ return [
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | IFrame
-    |--------------------------------------------------------------------------
-    |
-    | Here we change the IFrame mode configuration. Note these changes will
-    | only apply to the view that extends and enable the IFrame mode.
-    |
-    | For detailed instructions you can look the iframe mode section here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/IFrame-Mode-Configuration
-    |
-    */
-
     'iframe' => [
         'default_tab' => [
             'url' => null,
@@ -598,18 +340,6 @@ return [
             'use_navbar_items' => true,
         ],
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Livewire
-    |--------------------------------------------------------------------------
-    |
-    | Here we can enable the Livewire support.
-    |
-    | For detailed instructions you can look the livewire here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Other-Configuration
-    |
-    */
 
     'livewire' => false,
 ];
