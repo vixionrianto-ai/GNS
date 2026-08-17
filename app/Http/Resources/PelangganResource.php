9a9338cd25fdec48c6b5c1b37cf3ab1841b1cf6c
vixionrianto-ai <vixionrianto@gmail.com>
2026-08-17 22:43:50 +0700

    Add backend WhatsApp URL to pelanggan resource

--- app/Http/Resources/PelangganResource.php
+++ app/Http/Resources/PelangganResource.php
@@ -4,24 +4,33 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Services\WhatsAppService;
 
 class PelangganResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $whatsappUrl = null;
+
+        if (!empty($this->no_hp)) {
+            $whatsappUrl = app(WhatsAppService::class)->url(
+                $this->no_hp,
+                ''
+            );
+        }
+
         return [
             'id' => $this->id,
             'kode_pelanggan' => $this->kode_pelanggan,
             'nama' => $this->nama,
             'alamat' => $this->alamat,
             'no_hp' => $this->no_hp,
+            'whatsapp_url' => $whatsappUrl,
             'status' => $this->status,
             'tagihan_belum_lunas' => $this->tagihan_belum_lunas,
             'username_pppoe' => $this->username_pppoe,
             'password_pppoe' => $this->password_pppoe,
             'ip_address' => $this->ip_address,
-            
-            // --- TAMBAHKAN KUNCI PENTING INI AGAR TERBACA DI ANDROID ---
             'mac_address' => $this->mac_address,
             'router_id' => $this->router_id,
             'paket_id' => $this->paket_id,
@@ -30,7 +39,6 @@ class PelangganResource extends JsonResource
             'keterangan' => $this->keterangan,
             'isolation_use_default' => $this->isolation_use_default,
             'isolation_period_limit' => $this->isolation_period_limit,
-            
             'is_isolated' => (bool) $this->is_isolated,
 
             'paket' => $this->whenLoaded('paket', function () {
