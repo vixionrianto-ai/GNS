465b2203aabbbc5677f7c9468547c6762a1d7f83
vixionrianto-ai <vixionrianto@gmail.com>
2026-08-19 11:31:13 +0700

    Use framework-native User model without Sanctum dependency

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,13 +8,12 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     protected function casts(): array
     {
