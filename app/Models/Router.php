f93eb3612df4d993ef2ae56a3c7f84dd414d32bb
vixionrianto-ai <vixionrianto@gmail.com>
2026-09-05 21:12:42 +0700

    Hide router credentials from serialized API output

--- app/Models/Router.php
+++ app/Models/Router.php
@@ -7,32 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Router extends Model
 {
     protected $fillable = [
+        'nama_router',
+        'ip_router',
+        'api_port',
+        'username',
+        'password',
+        'lokasi',
+        'versi_routeros',
+        'identity',
+        'ssl',
+        'status',
+    ];
 
-    'nama_router',
-
-    'ip_router',
-
-    'api_port',
-
-    'username',
-
-    'password',
-
-    'lokasi',
-
-    'versi_routeros',
-
-    'identity',
-
-    'ssl',
-
-    'status',
-
-];
+    protected $hidden = [
+        'username',
+        'password',
+    ];
 
     public function pelanggans()
     {
-     return $this->hasMany(Pelanggan::class);
+        return $this->hasMany(Pelanggan::class);
     }
 }
-    
